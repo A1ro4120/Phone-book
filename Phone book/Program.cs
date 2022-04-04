cd023d3d533155daa5e680ff1cfc32c1c7a00e39
Koloshva Artem <artemkoloshva11yt@gmail.com>
2022-04-04 23:01:00 +0300

    Добавьте файлы проекта.

---
+++ Phone book/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phone_book
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Поиск владельца по номеру телефона
+        /// </summary>
+        static void OwnerSearch(Dictionary<string, string> PhoneBook)
+        {
+            Console.WriteLine("Введите номер телефона владельца, которого хотите найти:");
+
+            string value = "";
+
+            if (PhoneBook.TryGetValue(Console.ReadLine(), out value))
+            {
+                Console.WriteLine(value);
+            }
+            else
+            {
+                Console.WriteLine("Владелец этого номера не найден");
+            }
+
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Ввод данных телефон / Ф.И.О.
+        /// </summary>
+        /// <param name="PhoneBook"></param>
+        static void DataInput(Dictionary<string, string> PhoneBook)
+        {
+            Console.WriteLine("Введите номер телефона и Ф.И.О. его владельца (чтобы пропустить введите пустую строку)");
+
+            string phoneNumber = " ";
+            string fullName = " ";
+
+            while (phoneNumber != string.Empty || fullName != string.Empty)
+            {
+                Console.Write("Номер телефона: ");
+
+                phoneNumber = Console.ReadLine();
+
+                Console.Write("Ф.И.О.: ");
+
+                fullName = Console.ReadLine();
+
+                PhoneBook.Add(phoneNumber, fullName);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> phoneBook = new Dictionary<string, string>();
+
+            DataInput(phoneBook);
+
+            OwnerSearch(phoneBook);
+        }
+    }
+}
---
+++ Phone book/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Phone book")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Phone book")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("ba6ea682-3e00-4c62-9880-1f2025f59304")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
